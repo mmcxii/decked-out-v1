52f709d50ae63e84d147b6870a90c746ceccc4aa
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-09-11 16:56:14 -0700

    Added Users table

---
+++ models/user.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes) {
+    const User = sequelize.define('User', {
+        username: {
+            type: DataTypes.STRING,
+            validate: {
+                len: [6, 32]
+            }
+        },
+        password: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [8, 64]
+            }
+        },
+        secret_answer: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        }
+
+    })
+
+    return User;
+}
+};
